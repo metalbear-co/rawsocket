dd4a4b3c257e84f454581b5c434f146bd41aab36
Aviram Hassan <aviramyhassan@gmail.com>
2022-10-08 08:04:37 +0000

    ..

---
+++ rawsocket/examples/capture.rs
@@ -0,0 +1,14 @@
+use bs_filter::SocketFilterProgram;
+use rawsocket::{self, RawCapture, filter::build_tcp_port_filter};
+
+#[tokio::main]
+async fn main() {
+    let filter = build_tcp_port_filter(&[1337, 2000, 3333, 51222]);
+    println!("{}", filter.to_dump());
+    let capture = RawCapture::from_interface_name("eth0").expect("capture creation failed");
+    capture.set_filter(filter).expect("setting filter failed");
+    loop {
+        let packet = capture.next().await.expect("packet failed");
+        println!("packet {packet:?}");
+    }
+}
