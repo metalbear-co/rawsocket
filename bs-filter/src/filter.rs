359745cce37e61a5482cfe675fb0b0ed12d7475e
Aviram Hassan <aviramyhassan@gmail.com>
2022-10-08 08:03:55 +0000

    ipv4 works

--- bs-filter/src/filter.rs
+++ bs-filter/src/filter.rs
@@ -52,7 +52,7 @@ pub fn build_tcp_port_filter(ports: &[u16]) -> SocketFilterProgram {
     // Load protocol from IPv6
     instructions.extend(load_u8_at(SIZE_ETHER_HEADER + OFFSET_IP6_NEXT_HEADER));
     // Check if Protocol is TCP, if not jump to drop
-    instructions.extend(jump(Comparison::Equal, IPPROTO_TCP as _, 0, 13 + ports.len() + 1 + ports.len() + 2));
+    instructions.extend(jump(Comparison::Equal, IPPROTO_TCP as _, 0, 13 + ports.len() + 1 + ports.len()));
     // Load src port into A then store it in M[0]
     instructions.extend(load_u16_at(SIZE_ETHER_HEADER + SIZE_IPV6_HEADER + OFFSET_TCP_SRC_PORT));
     instructions.extend(store_a_in_m_at(src_port_m_offset));
@@ -62,14 +62,14 @@ pub fn build_tcp_port_filter(ports: &[u16]) -> SocketFilterProgram {
     instructions.extend(jump(Comparison::Always, 0, 0, 10));
     // If we got here, it's from ether type check, so A has the ether type loaded
     // Check if it's IPv4, if not, drop
-    instructions.extend(jump(Comparison::Equal, ETH_P_IP, 0, 8 + ports.len() + 1 + ports.len() + 2));
+    instructions.extend(jump(Comparison::Equal, ETH_P_IP, 0, 8 + ports.len() + 1 + ports.len()));
     // Check for TCP
     instructions.extend(load_u8_at(SIZE_ETHER_HEADER + OFFSET_IP4_PROTO));
-    instructions.extend(jump(Comparison::Equal, IPPROTO_TCP as _, 0, 6 + ports.len() + 1 + ports.len() + 2));
+    instructions.extend(jump(Comparison::Equal, IPPROTO_TCP as _, 0, 6 + ports.len() + 1 + ports.len()));
     // Check for fragmentation
     instructions.extend(load_u16_at(SIZE_ETHER_HEADER + OFFSET_IP4_FRAGMENT));
     // Not sure how this check works but this is how it is..
-    instructions.extend(jump(Comparison::AndMask, 0x1fff, 4 + ports.len() + 1 + ports.len() + 2, 0));
+    instructions.extend(jump(Comparison::AndMask, 0x1fff, 4 + ports.len() + 1 + ports.len(), 0));
     // Load offset for tcp header - X = offset of fragment header in the packet, from there it's
     // 14 bytes for src port and 16 bytes to dst port.
     instructions.extend(load_u8_into_x_from_packet_at(14));
@@ -82,9 +82,8 @@ pub fn build_tcp_port_filter(ports: &[u16]) -> SocketFilterProgram {
     
     // Check dst port first (arleady in A)
     for (i, port) in ports.iter().enumerate() {
-        let accept = ports.len() + ports.len() - i + 1 - 1;
-        let drop = ports.len() + ports.len() - i + 2;
-        instructions.extend(jump(Comparison::Equal, *port as _, accept, drop));
+        let accept = ports.len() + ports.len() - i + 1;
+        instructions.extend(jump(Comparison::Equal, *port as _, accept, 0));
     }
 
     // Load src port into A then do the same check
@@ -92,13 +91,12 @@ pub fn build_tcp_port_filter(ports: &[u16]) -> SocketFilterProgram {
 
     // Check dst port first (arleady in A)
     for (i, port) in ports.iter().enumerate() {
-        let accept = ports.len() - i - 1;
-        let drop = ports.len() - i + 1;
-        instructions.extend(jump(Comparison::Equal, *port as _, accept, drop));
+        let accept = ports.len() - i;
+        instructions.extend(jump(Comparison::Equal, *port as _, accept, 0));
     }
     
-    instructions.extend(teotology());
     instructions.extend(contradiction());
+    instructions.extend(teotology());
 
     SocketFilterProgram::from_vector(instructions)
 
